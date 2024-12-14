74d51993aaa87657b1741630c5a04ac96f32fa67
MennaAtef8240 <mennaatef80747@gmail.com>
2024-12-13 23:06:25 -0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('learn-more').addEventListener('click', () => {
+    alert('Explore our wide range of courses to start learning today!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for reaching out to us. We will get back to you shortly!');
+});
